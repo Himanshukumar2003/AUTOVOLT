69791c005aedbfd7da194fff82e073924a62c876
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-05-16 12:55:05 +0530

    first commit

--- index.js
+++ index.js
@@ -45,3 +45,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// panels
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => panel.classList.remove("active"));
+}
